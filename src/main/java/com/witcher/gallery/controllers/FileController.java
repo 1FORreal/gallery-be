7c81fb1682dbdef03c29c65e95a770d5614680e5
Cezar <biculescu.cezar@gmail.com>
2022-08-02 12:12:06 +0300

    Adding spring security and loombok dependencies

--- src/main/java/com/witcher/gallery/controllers/FileController.java
+++ src/main/java/com/witcher/gallery/controllers/FileController.java
@@ -2,7 +2,6 @@ package com.witcher.gallery.controllers;
 
 import com.witcher.gallery.services.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
--- src/test/java/com/witcher/gallery/GalleryApplicationTests.java
+++ src/test/java/com/witcher/gallery/GalleryApplicationTests.java
@@ -1,10 +1,22 @@
 package com.witcher.gallery;
 
+import com.witcher.gallery.repositories.PhotoRepository;
+import com.witcher.gallery.services.PhotoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
 class GalleryApplicationTests {
+
+	@MockBean
+	private PhotoRepository photoRepository;
+
+	@Autowired
+	private PhotoService photoService;
 
 	@Test
 	void contextLoads() {
