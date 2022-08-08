f1287aa1c96606e1c4fded98a654fab38402ba5f
Cezar <biculescu.cezar@gmail.com>
2022-08-08 17:41:38 +0300

    Add authorities to user

--- src/main/java/com/witcher/gallery/security/SecurityUser.java
+++ src/main/java/com/witcher/gallery/security/SecurityUser.java
@@ -24,7 +24,7 @@ public class SecurityUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> "read");
+        return List.of(() -> "read", () -> "create",() -> "update", () -> "delete");
     }
 
     @Override
