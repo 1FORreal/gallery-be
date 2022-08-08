9fac642ecad3037e23eee979544dbe771195da9b
Cezar <biculescu.cezar@gmail.com>
2022-08-08 17:29:42 +0300

    User credentials for authentication

--- src/main/java/com/witcher/gallery/project_config/SecurityConfig.java
+++ src/main/java/com/witcher/gallery/project_config/SecurityConfig.java
@@ -4,16 +4,30 @@ import com.witcher.gallery.repositories.UserRepository;
 import com.witcher.gallery.services.JpaUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.annotation.Resource;
+
 @Configuration
 public class SecurityConfig {
+
+    @Resource
+    private JpaUserDetailsService jpaUserDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(this.jpaUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
     }
 }
 
